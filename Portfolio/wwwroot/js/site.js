8f12d865cf5fb4f83ff58c898f6b057f54f712c7
Re4derTheGlorious <karol.re4der@gmail.com>
2025-04-10 15:06:10 +0200

    Single photo display scaling

--- Portfolio/wwwroot/js/site.js
+++ Portfolio/wwwroot/js/site.js
@@ -1,11 +1,30 @@
-﻿//Handle single photo display for gallery
+﻿var imageWidth = 0;
+var imageHeight = 0;
 const modal = document.getElementById('singlePhotoModal')
+
+function resizeModal() {
+    if (modal) {
+        var windowWidth = window.innerWidth;
+        var windowHeight = window.innerHeight;
+
+        if (imageWidth < windowWidth && imageHeight < windowHeight) {
+            modal.querySelector('.modal-dialog').style = 'max-width: ' + imageWidth + 'px; max-height: ' + imageHeight + 'px; height:'+imageHeight+'px';
+        }
+        else{
+            modal.querySelector('.modal-dialog').style = 'max-width: ' + (imageWidth / imageHeight) * windowHeight + 'px; max-height: ' + windowHeight + 'px; height:' + windowHeight+'px';
+        }
+    }
+}
+
 if (modal) {
     modal.addEventListener('show.bs.modal', event => {
         const imagePath = event.relatedTarget.getAttribute('data-bs-path')
+        imageWidth = event.relatedTarget.getAttribute('data-bs-width')
+        imageHeight = event.relatedTarget.getAttribute('data-bs-height')
 
-        const image = modal.querySelector('.modal-body img')
+        resizeModal();
 
-        image.src = imagePath
+        modal.querySelector('.modal-body img').src = imagePath
     })
 }
+window.addEventListener('resize', resizeModal);
