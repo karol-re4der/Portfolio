040a64a9e954bf0c6069db66ed57800227bf8ba3
Re4derTheGlorious <karol.re4der@gmail.com>
2025-04-12 19:55:51 +0200

    Contact page mockup

--- Portfolio/Controllers/HomeController.cs
+++ Portfolio/Controllers/HomeController.cs
@@ -26,6 +26,11 @@ namespace Portfolio.Controllers
             return View(model);
         }
 
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
